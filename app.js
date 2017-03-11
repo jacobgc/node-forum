2e3095be84af5a18a09b5b039c9cd5ddaac3706f
jakegc <jacob.clarke2@gmail.com>
2017-03-11 14:17:46 +0000

    Added some S3 thumbnial stuff

--- app.js
+++ app.js
@@ -8,6 +8,8 @@ var bodyParser = require('body-parser');
 var routes = require('./routes/index');
 var users = require('./routes/users');
 
+var aws = require('aws-sdk'); // Amazon <3
+
 var app = express();
 
 // view engine setup
--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,65 @@
 var express = require('express');
 var router = express.Router();
+var http = require('http');
+var fs = require('fs');
+var bcrypt = require('bcrypt-nodejs');
+var sharp = require('sharp');
+var r = require('rethinkdb');
 
-/* GET home page. */
+var AWS = require('aws-sdk'); // Amazon <3
+// Create an S3 client
+var s3 = new AWS.S3({ signatureVersion: 'v4' });
+
+var connection = null;
+r.connect({ host: 'localhost', port: 28015 }, function(err, conn) {
+    if (err) throw err;
+    connection = conn;
+})
+
+
+// /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+    res.render('index', { title: 'Express' });
+});
+
+
+
+
+
+
+router.get('/thumbnail', function(req, res, next) { // if someone goes to /thumbnail
+    var url = req.query.url; // get the URL from the query
+    url = decodeURI(url); // decode the URL
+    if (!url.match(/^(http|https):\//)) { // if the url does not start with http://
+        url = "http://" + url; // add http:// to it
+    }
+    var fType = url.match(/\.[0-9a-z]+$/i); // Get the URLs filetype
+
+    http.get(url, function(response) { // GET the data from the url given
+        var etag = response.headers.etag.replace(/(?:")+/g, ""); // Remove quotes from the etag
+        response.pipe(sharp() // use sharp to resize the image to a thumbnail
+            .resize(200, 200)
+            .toBuffer(function(err, buffer) {
+                var bucketName = 'shreddit'; // send it over to S3
+                var keyName = url.replace(/^(http|https):\/\//, ""); // give it a name (url without http(s)://)
+                s3.createBucket({ Bucket: bucketName }, function() {
+                    var params = { Bucket: bucketName, Key: keyName, Body: buffer, ACL: 'public-read' }; // Make sure public can read with ACL public-read
+                    s3.putObject(params, function(err, data) { // Put the file on S3
+                        if (err)
+                            throw err;
+                        else
+                            r.db('shreddit').table('imageStore').insert([{ // Add some meta data do the DB of where the image is
+                                url: url, // Original URL of the file
+                                etag: etag, // Etag cache key
+                                s3Loc: "https://s3.eu-west-2.amazonaws.com/shreddit/" + url // Location on S3
+                            }]).run(connection, function(err, result) {
+                                if (err) throw err;
+                            })
+                        res.end();
+                    });
+                });
+            }));
+    });
 });
 
 module.exports = router;
