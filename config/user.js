fd62bef44a82b60c659bd2c1779f278e0ad9e1de
jakegc <jacob.clarke2@gmail.com>
2017-03-14 23:47:39 +0000

    whoops

--- config/user.js
+++ config/user.js
@@ -1,6 +1,7 @@
 var bcrypt = require('bcrypt-nodejs');
 var r = require('rethinkdb');
 
+
 var connection = null;
 r.connect({ host: 'localhost', port: 28015 }, function(err, conn) {
     if (err) throw err;
